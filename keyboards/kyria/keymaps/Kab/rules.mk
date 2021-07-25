084afa104142f305f44c8a145608ac5e36ee18e5
Kab Thangarajah <kabilanm.thangarajah@gmail.com>
2021-07-24 20:55:41 -0700

    Adjusted default to have bongo cat :D

---
+++ keyboards/kyria/keymaps/Kab/rules.mk
@@ -0,0 +1,11 @@
+OLED_DRIVER_ENABLE = yes   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+WPM_ENABLE = yes
+
+AUDIO_ENABLE = no
+CONSOLE_ENABLE = no
+MOUSEKEY_ENABLE = no
+EXTRAFLAGS += -flto
+RGBLIGHT_ANIMATIONS = no
+SPACE_CADET_ENABLE = no
